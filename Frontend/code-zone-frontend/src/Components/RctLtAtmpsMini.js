e7927a698d5c517bf393900abe0396649943b48d
Elysse Viena <elysseviena23@gmail.com>
2024-10-30 15:43:35 -0400

    added icon library and started some components

--- Frontend/code-zone-frontend/src/Components/RctLtAtmpsMini.js
+++ Frontend/code-zone-frontend/src/Components/RctLtAtmpsMini.js
@@ -1,10 +1,24 @@
 import {Link} from "react-router-dom"
 import "./RctLtAtmpsMini.css"
+import { CiFaceFrown } from "react-icons/ci";
+import { CiFaceMeh } from "react-icons/ci";
+import { CiFaceSmile } from "react-icons/ci";
+
+
 function RctLtAtmpsMini({atmptData}) {
     return(
         <>
             <div className="box">
                 <h2>{atmptData.name}</h2>
+                <h3>Difficulty</h3>
+                <section className={"layout"}>
+                    <div><CiFaceFrown /></div>
+                    <div><CiFaceMeh /></div>
+                    <div><CiFaceSmile /></div>
+                </section>
+                <h3>Comments</h3>
+                <p>{atmptData.comments}</p>
+                <a href={`${atmptData.link}`} target={"_blank"}>click here!</a>
             </div>
         </>
     );
