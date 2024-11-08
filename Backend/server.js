dd526a838662c859930c49be01c3ba19b734e281
Erick Pinedo <124737187+Erickferpinedo@users.noreply.github.com>
2024-11-08 17:14:10 -0500

    .

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,22 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from  'mongoose'
+
+
 const app = express(); 
 const PORT = process.env.PORT || 3000; 
 
-// Basic route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+dotenv.config(); // Load environment variables from .env file
+
+const dbConnectionString = process.env.DB_CONNECTION_STRING; // MongoDB connection string
+
+mongoose.connect(dbConnectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("MongoDB connection error:", err));
+
 
 // Start the server
 app.listen(PORT, () => {
