0fdad8ec213e52546f648421bc782532c97ebd2c
GeorgeU029 <georgeulloa.fl@gmail.com>
2024-12-03 12:33:17 -0500

    log problem and notes update

--- code-zone-frontend/src/components/log-prob-components/Notes.js
+++ code-zone-frontend/src/components/log-prob-components/Notes.js
@@ -14,17 +14,8 @@ const Notes = () => {
         <div className="notes-content">
           <p className="notes-title"># Notes</p>
           <div className="notes-lines">
-            <div className="notes-line"></div>
             <div contenteditable="true"></div>
-            <div className="notes-line"></div>
-            <div contenteditable="true"></div>
-            <div className="notes-line"></div>
-            <div contenteditable="true"></div>
-            <div className="notes-line"></div>
-            <div contenteditable="true"></div>
-            <div className="notes-line"></div>
-            <div contenteditable="true"></div>
-            <div className="notes-line"></div>
+          
           </div>
         </div>
       </div>
