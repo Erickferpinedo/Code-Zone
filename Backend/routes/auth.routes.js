3d1304406770aafc43c1e5751b987e18650c6489
Ant1 <47747877+Ant1-dev@users.noreply.github.com>
2024-12-06 07:03:50 -0500

    added keep session info to google auth for testing

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // Route to initiate Google authentication
 router.get(
   "/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
+  passport.authenticate("google", { scope: ["profile", "email"], keepSessionInfo: true })
 );
 
 //route for github
